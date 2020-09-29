4374bd5fc10777783ead29579de1249ae96ca3dd
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2020-09-29 17:01:18 +0200

    Ajout du contenu du header

--- wp-content/themes/now/header.php
+++ wp-content/themes/now/header.php
@@ -4,16 +4,27 @@
  *
  * @package WordPress
  * @subpackage now
+ * @since Now 1.0.0
  */
 
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-    
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
     <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
+    <div id="page" class="hfeed site">
+        <!-- Header -->
+        <header id="masthead" class="site-header" role="banner">
+
+            <?php do_action('now_header'); ?>
+        
+        </header><!-- #masthead -->
+
+        <div class="main-area">
