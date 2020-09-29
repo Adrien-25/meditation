1daa9842df6e5fdc8bc7a4577738557d460c4637
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2020-09-29 16:53:22 +0200

    Ajout structure de base
    Ajout de la structure de base dans les pages 404, archive, front-page et single

--- wp-content/themes/now/404.php
+++ wp-content/themes/now/404.php
@@ -3,6 +3,6 @@
  * The template for displaying 404 pages (Not Found)
  *
  * @package WordPress
- * @subpackage now.
- * @since Now.
+ * @subpackage now
+ * @since Now 1.0.0
  */
--- wp-content/themes/now/front-page.php
+++ wp-content/themes/now/front-page.php
@@ -7,3 +7,12 @@
  */
 
 get_header(); ?>
+    <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+        
+        <h1><?php the_title(); ?></h1>
+
+        <?php the_content(); ?>
+
+    <?php endwhile; endif; ?>
+
+<?php get_footer(); ?>
