7fd82f3b4b2c641184f735f66a6b63ed525d58e2
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2020-10-06 16:56:19 +0200

    Ajout du contenu du footer
    Ajout du contenu du footer avec une action qui va executer la fonction qui affiche le footer dans function.php

--- wp-content/themes/now/footer.php
+++ wp-content/themes/now/footer.php
@@ -7,7 +7,16 @@
  */
 
 ?>
-<?php wp_footer(); ?>
-</body>
+    </div> <!-- .main-area -->
+    <?php
+    if ( ( ! is_front_page() || '1' == now_get_theme_mod( 'is_home_footer' )) && ! is_page_template( 'page-templates/no-content-footer.php' ) ) 
+    ?>
 
+    <footer id="colophon" class="site-footer">
+        <?php do_action('now_site_info'); ?>
+    </footer><!-- #colophon -->
+	</div><!-- #page -->
+	<?php wp_footer(); ?>
+
+</body>
 </html>
