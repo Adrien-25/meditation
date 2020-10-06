dbbaeae82e6439c13c3665b68e507a3062db1e11
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2020-10-06 12:10:01 +0200

    ajout de l'action qui va calculer le nombre de fois ou l'article a été vue

--- wp-content/themes/now/single.php
+++ wp-content/themes/now/single.php
@@ -10,26 +10,34 @@ get_header(); ?>
 
 <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
 
-<article class="post">
-    <h1><?php the_title(); ?></h1>
-    
-    <?php the_post_thumbnail(); ?>
+    <?php 
+    do_action('setPostViews', get_the_ID());
+    // $view = wpb_get_post_views(get_the_ID());
+    // echo 'Nombre de vues : '.get_post_meta(get_the_ID(), 'wpb_post_views_count', true);
+    ?>
+
+    <article class="post">
+        <h1><?php the_title(); ?></h1>
+        
+        <?php the_post_thumbnail(); ?>
 
 
-    <div class="post__meta">
-        <?php echo get_avatar( get_the_author_meta( 'ID' ), 40 ); ?>
-        <p>
-            Publié le <?php the_date(); ?>
-            par <?php the_author(); ?>
-            Dans la catégorie <?php the_category(); ?>
-            Avec les étiquettes <?php the_tags(); ?>
-        </p>
-    </div>
+        <div class="post__meta">
+            <?php echo get_avatar( get_the_author_meta( 'ID' ), 40 ); ?>
+            <p>
+                Publié le <?php the_date(); ?>
+                par <?php the_author(); ?>
+                Dans la catégorie <?php the_category(); ?>
+                Avec les étiquettes <?php the_tags(); ?>
+                <?php // echo $view; ?>
+            </p>
+        </div>
 
-    <div class="post__content">
-        <?php the_content(); ?>
-    </div>
-</article>
+        <div class="post__content">
+            <?php the_content(); ?>
+        </div>
+
+    </article>
 
 <?php endwhile; endif; ?>
 <?php get_footer(); ?>
